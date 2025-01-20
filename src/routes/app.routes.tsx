d98c2ddba82240f1ab68487ddbec883f56caf6d3
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2025-01-20 20:46:00 -0300

    create home

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,86 @@
+import {
+  BottomTabNavigationProp,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import { Exercise } from "@screens/exercise";
+import { History } from "@screens/history";
+import { Home } from "@screens/home";
+import { Profile } from "@screens/profile";
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
+import { Platform } from "react-native";
+
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+};
+
+export type AppNavigatorRouterProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+  const { tokens } = gluestackUIConfig;
+  const iconSize = tokens.space["6"];
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false, // remove o nome da rota do tab
+        tabBarInactiveTintColor: tokens.colors.gray200, // cor do tab inativo
+        tabBarActiveTintColor: tokens.colors.green500, // cor do tab ativo
+        tabBarStyle: {
+          backgroundColor: tokens.colors.gray600, // cor de fundo do tab bar
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: tokens.space["10"],
+          paddingTop: tokens.space["6"],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          // muda o icon do tab
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={iconSize} height={iconSize} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg width={iconSize} height={iconSize} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg width={iconSize} height={iconSize} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{
+          tabBarButton: () => null,
+          tabBarItemStyle: {
+            display: "none",
+          },
+        }} // remove o button
+      />
+    </Navigator>
+  );
+}
---
+++ src/screens/home.tsx
@@ -0,0 +1,61 @@
+import { ExerciseCard } from "@components/exerciseCard";
+import { Group } from "@components/group";
+import { HomeHeader } from "@components/homeHeader";
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+export function Home() {
+  const [groups, setGroups] = useState(["Costas", "Peito", "Ombro", "Pernas"]);
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terra",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            title={item}
+            isActive={
+              groupSelected.toLocaleLowerCase() === item.toLocaleLowerCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
+  );
+}
